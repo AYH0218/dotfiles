e189376229ca1cd6d20b4f7d44229ef71f5f1966
Ayh <43546428+AYH0218@users.noreply.github.com>
2025-09-28 20:39:28 +0900

    Update colorscheme.lua

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,40 +1,77 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      flavour = "mocha",
-      background = {
-        light = "latte",
-        dark = "mocha",
-      },
-      transparent_background = true,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        telescope = true,
-        notify = true,
-        mini = true,
-      },
-    },
-  },
+	{
+		"folke/tokyonight.nvim",
+		opts = {
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			flavour = "mocha",
+			background = {
+				light = "latte",
+				dark = "mocha",
+			},
+			transparent_background = true,
+			float = {
+				transparent = true, -- enable transparent floating windows
+				-- solid = false, -- use solid styling for floating windows, see |winborder|
+			},
+			default_integrations = true,
+			auto_integrations = false,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				telescope = true,
+				notify = true,
+				mini = true,
+				neotree = true,
+				bufferline = true,
+			},
+		},
+	},
 
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
-    },
-  },
+	{
+		"EdenEast/nightfox.nvim",
+		name = "nightfox",
+		priority = 1000,
+		opts = {
+			-- 使用するバリアント (nightfox, dayfox, duskfox, nordfox, terafox)
+			variant = "duskfox",
+			options = {
+				transparent = true,
+				styles = {
+					comments = "italic",
+					keywords = "bold",
+				},
+			},
+			palettes = {
+				duskfox = {
+					-- bg1 = "#000000", -- Black background
+					-- bg0 = "NONE", -- Alt backgrounds (floats, statusline, ...)
+					-- bg3 = "#121820", -- 55% darkened from stock
+					-- sel0 = "#131b24", -- 55% darkened from stock
+					-- sel1 = "#131b24", -- 55% darkened from stock
+					-- fg0 = "#000000",
+					-- fg1 = "#000000",
+					-- fg2 = "#121820",
+					-- fg3 = "#000000",
+				},
+			},
+		},
+	},
+
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
 }
