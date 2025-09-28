3685a57766555035701daf4c88bb752d9178742c
Ayh <43546428+AYH0218@users.noreply.github.com>
2025-09-28 20:35:54 +0900

    Update render-markdown.lua

--- config/nvim/lua/plugins/render-markdown.lua
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -1,10 +1,47 @@
 return {
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = {
-      checkbox = {
-        enabled = true,
-      },
-    },
-  },
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {
+			checkbox = {
+				enabled = true,
+			},
+			heading = {
+				enabled = true,
+				render_modes = true,
+				atx = true,
+				setext = true,
+				sign = true,
+				icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+				position = "overlay",
+				signs = { "󰫎 " },
+				width = "full",
+				left_margin = 0,
+				left_pad = 0,
+				right_pad = 0,
+				min_width = 0,
+				border = false,
+				border_virtual = false,
+				border_prefix = true,
+				above = "▄",
+				below = "▀",
+				backgrounds = {
+					"RenderMarkdownH1Bg",
+					"RenderMarkdownH2Bg",
+					"RenderMarkdownH3Bg",
+					"RenderMarkdownH4Bg",
+					"RenderMarkdownH5Bg",
+					"RenderMarkdownH6Bg",
+				},
+				foregrounds = {
+					"RenderMarkdownH1",
+					"RenderMarkdownH2",
+					"RenderMarkdownH3",
+					"RenderMarkdownH4",
+					"RenderMarkdownH5",
+					"RenderMarkdownH6",
+				},
+				custom = {},
+			},
+		},
+	},
 }
